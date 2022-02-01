df03177489b02754fbbc097a01f30637bd76cdab
Md Abu Zafar <mdabuzafar78@gmail.com>
2022-02-01 22:56:15 +0530

    feat: Create the cart drop down
    - Update the reducer to handle add items to cart
    - Make item update in cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,15 +7,21 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, cartItems }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{cartItems.length}</span>
   </div>
 );
+
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  return {
+    cartItems,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,18 @@
+export const addItemToCart = (itemsInCart, itemToAdd) => {
+  const existingCartItem = itemsInCart.find(
+    (itemInCart) => itemInCart.id === itemToAdd.id,
+  );
+
+  if (existingCartItem) {
+    return itemsInCart.map((itemInCart) => {
+      return itemInCart.id === itemToAdd.id
+        ? {
+            ...itemInCart,
+            quantity: itemInCart.quantity + 1,
+          }
+        : itemInCart;
+    });
+  }
+
+  return [...itemsInCart, { ...itemToAdd, quantity: 1 }];
+};
