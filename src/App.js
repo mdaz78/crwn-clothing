57ef9623e4d6773ce785b60d7209d0a30654535d
Md Abu Zafar <mdabuzafar78@gmail.com>
2022-02-02 10:11:20 +0530

    feat: Create the checkout page

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import { createStructuredSelector } from 'reselect';
 import './App.css';
 
 import Header from './components/folder/header.component';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './components/checkout/checkout.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
@@ -55,6 +57,7 @@ class App extends React.Component {
               this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUp />
             }
           />
+          <Route exact path='/checkout' component={CheckoutPage} />
         </Switch>
       </div>
     );
