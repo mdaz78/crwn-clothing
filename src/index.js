9da71bd7279b87db1b809ed6b40d5fc7438c5c75
Md Abu Zafar <mdabuzafar78@gmail.com>
2022-02-04 12:53:54 +0530

    feat: Add react-persist
    - Add react persist and preserve the cart on each refresh

--- src/index.js
+++ src/index.js
@@ -8,13 +8,17 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import store from './redux/store';
+
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
