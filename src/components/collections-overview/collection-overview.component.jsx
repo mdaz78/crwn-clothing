2b783a1b00911333ec4a3c84f07911f49b585f03
Md Abu Zafar <mdabuzafar78@gmail.com>
2022-02-15 16:37:38 +0530

    refactor: Update the collection-overview component to use styled-component

--- src/components/collections-overview/collection-overview.component.jsx
+++ src/components/collections-overview/collection-overview.component.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import {
-  selectCollectionForPreview,
-  selectCollections,
-} from '../../redux/shop/shop.selectors';
+import { selectCollectionForPreview } from '../../redux/shop/shop.selectors';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
+import { CollectionsOverviewContainer } from './collection-overview.styles';
 
 const CollectionsOverview = ({ collections }) => {
   return (
-    <div className='collections-overview'>
+    <CollectionsOverviewContainer>
       {collections.map(({ id, ...otherCollectionProps }) => (
         <CollectionPreview key={id} {...otherCollectionProps} />
       ))}
-    </div>
+    </CollectionsOverviewContainer>
   );
 };
 
