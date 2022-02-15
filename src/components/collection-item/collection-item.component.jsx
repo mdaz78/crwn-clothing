153a03b5f5c1b9e612c0b8595b87fffc48df2f5f
Md Abu Zafar <mdabuzafar78@gmail.com>
2022-02-15 16:23:07 +0530

    refactor: collection-item uses styled-component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import './collection-item.styles.scss';
-
-import CustomButton from '../custom-button/custom-button.component';
-
 import { addItem } from '../../redux/cart/cart.action';
+import {
+  AddButton,
+  BackgroundImage,
+  CollectionFooterContainer,
+  CollectionItemContainer,
+  NameContainer,
+  PriceContainer,
+} from './collection-item.styles';
 
 const CollectionItem = ({ item, addItem }) => {
   const { imageUrl, name, price } = item;
   return (
-    <div className='collection-item'>
-      <div
-        className='image'
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
+    <CollectionItemContainer>
+      <BackgroundImage className='image' imageUrl={imageUrl} />
 
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>${price}</span>
-      </div>
-      <CustomButton inverted onClick={() => addItem(item)}>
+      <CollectionFooterContainer>
+        <NameContainer>{name}</NameContainer>
+        <PriceContainer>${price}</PriceContainer>
+      </CollectionFooterContainer>
+      <AddButton inverted onClick={() => addItem(item)}>
         Add to cart
-      </CustomButton>
-    </div>
+      </AddButton>
+    </CollectionItemContainer>
   );
 };
 
