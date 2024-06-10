f6222dfedd8412ac1ad803a5a5482febc954289a
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-10 06:52:22 +0530

    refactor: update App.js to use Directory component for rendering categories

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
-function App() {
-  const categories = [
-    {
-      title: "Hats",
-      id: 1,
-    },
-    {
-      title: "Jackets",
-      id: 2,
-    },
-    {
-      title: "Sneakers",
-      id: 3,
-    },
-    {
-      title: "Women",
-      id: 4,
-    },
-    {
-      title: "Mens",
-      id: 5,
-    },
-  ];
+import Directory from "./components/directory/directory.component";
+import { CATEGORIES } from "./config/categories.config";
 
-  return (
-    <div className="categories-container">
-      {categories.map(({ id, title }) => (
-        <div className="category-container" key={id}>
-          <div className="background-image"></div>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+function App() {
+  return <Directory categories={CATEGORIES} />;
 }
 
 export default App;
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CategoryItem from "../category-item/category-item.component";
+
+import "./directory.styles.scss";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map(({ id, title, imageUrl }) => (
+        <CategoryItem key={id} title={title} imageUrl={imageUrl} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
