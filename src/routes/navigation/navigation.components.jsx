f6c7becf5b74f0a98119f2375d2bb1ba51eafcbc
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-11 13:21:50 +0530

    refactor: Update navigation component to include logo and navigation links

--- src/routes/navigation/navigation.components.jsx
+++ src/routes/navigation/navigation.components.jsx
@@ -1,14 +1,23 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import "./navigation.styles.scss";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <h1>Navigation</h1>
+    <>
+      <div className="navigation">
+        <Link to="/" className="logo-container">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link to="/shop" className="nav-link">
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
-    </div>
+    </>
   );
 };
 
