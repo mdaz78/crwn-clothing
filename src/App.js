0c9bc34e26b93abb5c1cf5050f8cb5da593e6777
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-12 16:03:16 +0530

    feat: Update sign-in route to use new authentication component and auth route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import SignIn from "./routes/authentication/authentication.component";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,24 @@
+import SignInForm from "../../components/sign-in-form/sign-in-form.components";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import {
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+} from "../../utils/firebase/firebase.utils";
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(response.user);
+  };
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignIn;
