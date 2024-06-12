38049987f9fe563dacf1d7f56657eb3fc14e1f8e
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-12 11:33:31 +0530

    refactor: Update App.js to include sign-in route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.components";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
